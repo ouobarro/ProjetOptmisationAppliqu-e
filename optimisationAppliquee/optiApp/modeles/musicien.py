b812b05d439aabb5a8b8a2732f3c427b99f4ca35
Mohamed Sylla <momisylla@gmail.com>
2022-11-19 12:54:45 +0100

    recupération des données du formulaire et enregistrement dans le fichier

--- optimisationAppliquee/optiApp/modeles/musicien.py
+++ optimisationAppliquee/optiApp/modeles/musicien.py
@@ -15,67 +15,67 @@ class Musicien :
         self.styleMin = styleMin
 
     def get_name(self):
-        return self._name
+        return self.name
 
     def set_name(self, name):
         self.name=name
     
     def get_heureHebdo(self):
-        return self._heureHebdo
+        return self.heureHebdo
 
     def set_heureHebdo(self, heureHebdo):
         self.heureHebdo=heureHebdo
     
     def get_niveau(self):
-        return self._niveau
+        return self.niveau
 
     def set_niveau(self, niveau):
         self.niveau=niveau
         
     def get_instru(self):
-        return self._instru
+        return self.instru
 
     def set_instru(self, instru):
         self.instru=instru
     
     def get_ville(self):
-        return self._ville
+        return self.ville
 
     def set_ville(self, ville):
         self.ville=ville
         
     def get_style(self):
-        return self._style
+        return self.style
 
     def set_style(self, style):
         self.style=style
     
     def get_heureMin(self):
-        return self._heureMin
+        return self.heureMin
 
     def set_heureMin(self, heureMin):
         self.heureMin=heureMin
     
     def get_heureMax(self):
-        return self._heureMax
+        return self.heureMax
 
     def set_heureMax(self, heureMax):
         self.heureMax=heureMax
     
     def get_nivMin(self):
-        return self._nivMin
+        return self.nivMin
 
     def set_nivMin(self, nivMin):
         self.nivMin=nivMin
         
     def get_nivMax(self):
-        return self._nivMax
+        return self.nivMax
 
     def set_nivMax(self, nivMax):
         self.nivMax=nivMax
     
     def get_styleMin(self):
-        return self._styleMin
+        return self.styleMin
 
     def set_styleMin(self, styleMin):
         self.styleMin=styleMin
--- optimisationAppliquee/optiApp/views.py
+++ optimisationAppliquee/optiApp/views.py
@@ -1,9 +1,10 @@
 
 from optiApp.modeles.modele import resolutionContrainte 
-from optiApp.modeles.musiceinsList import genMusiciens 
+from optiApp.modeles.musiceinsList import genMusiciens,modifiyMusicienData
 from django.shortcuts import render
 #from django.http import HttpResponse
-
+from optiApp.modeles.musicien import * 
+from optiApp.modeles.generateData import *
 # Create your views here.
 def index(request):
     musiciens = genMusiciens()
@@ -16,9 +17,15 @@ def index(request):
 
 
 def afficheDuo(request):
+    print(request.POST.get('Alex-heureHebdo'))
     musiciens = genMusiciens()
-    
-    result_str=resolutionContrainte("projetDuoMusicien.mzn","projetDuoMusicien.dzn")
+    #print("avant",musiciens[0].get_heureHebdo())
+    musiciens = modifiyMusicienData(musiciens,request.POST)
+    #print("après",musiciens[0].get_heureHebdo())
+    result_str=""
+    fichierPret =genereFichier(generateData(musiciens))
+    if(fichierPret):
+        result_str=resolutionContrainte("projetDuoMusicien.mzn","projetDuoMusicien.dzn")
     context = {
         'propositionDuo': result_str,
          'musiciens': musiciens,
