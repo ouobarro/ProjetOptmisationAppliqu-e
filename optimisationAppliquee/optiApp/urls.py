1caad43f13a5c9ea668e92fa13e7142b696ac866
Mohamed Sylla <momisylla@gmail.com>
2022-11-21 09:43:28 +0100

    mise en forme du html et ducss

--- optimisationAppliquee/optiApp/urls.py
+++ optimisationAppliquee/optiApp/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('accueil/', views.index, name='index'),
     path('duo', views.afficheDuo, name='duo'),
+    path('', views.index, name='index'),
     
 ]
